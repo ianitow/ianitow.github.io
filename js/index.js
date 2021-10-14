76e25771b5625235e6d4a13f8e01cc8cf870dc4a
ianito <iaan.developer@gmail.com>
2021-10-14 01:36:52 -0300

    [RELEASED] Changes 2021

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function typeWriter(elementID, text) {
+  let i = 0;
+  function write() {
+    var speed = 20;
+    if (i < text.length) {
+      const a = (document.getElementById(elementID).innerHTML +=
+        text.charAt(i));
+      console.log(document.getElementById(elementID));
+      i++;
+      setTimeout(write, speed);
+    }
+  }
+  write();
+}
+
+document.querySelectorAll("ul#menu-content li a").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector("input[type=checkbox").checked = false;
+  });
+});
+typeWriter("type-name", "DESENVOLVEDOR FULL-STACK");
